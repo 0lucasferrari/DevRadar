12cd43ddcfade56e403d1081781a091bd11b1636
0lucasferrari <lucas.p.f.carvalho@gmail.com>
2020-01-16 23:02:10 -0300

    Construindo a interface web - parte 3

--- web/src/App.js
+++ web/src/App.js
@@ -4,172 +4,37 @@ import './global.css';
 import './App.css';
 import './Sidebar.css';
 import './Main.css';
+import DevItem from './components/DevItems';
+import DevForm from './components/DevForm';
 
 function App() {
-  const [latitude, setLatitude] = useState('');
-  const [longitude, setLongitude] = useState('');
-  const [github_username, setGithubUsername] = useState('');
-  const [techs, setTechs] = useState('');
-
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition(
-      position => {
-        const { latitude, longitude } = position.coords;
-
-        setLatitude(latitude);
-        setLongitude(longitude);
-      },
-      err => {
-        console.log(err);
-      },
-      {
-        timeout: 30000
-      }
-    );
-  }, []);
+  const [devs, setDevs] = useState([]);
 
   useEffect(() => {
     async function loadDevs() {
-      const response = await api.get('dev');
+      const response = await api.get('/devs');
+      setDevs(response.data);
     }
+    loadDevs();
   }, []);
 
-  async function handleAddDev(e) {
-    e.preventDefault();
+  async function handleAddDev(data) {
+    const response = await api.post('/devs', data);
 
-    const response = await api.post('/devs', {
-      github_username,
-      techs,
-      latitude,
-      longitude
-    });
-
-    setGithubUsername('');
-    setTechs('');
+    setDevs([...devs, response.data]);
   }
 
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form onSubmit={handleAddDev}>
-          <div className="input-block">
-            <label htmlFor="github_username">Usuário do Github</label>
-            <input
-              name="github_username"
-              id="github_username"
-              value={github_username}
-              onChange={e => setGithubUsername(e.target.value)}
-              required
-            />
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="techs">Tecnologias</label>
-            <input
-              name="techs"
-              id="techs"
-              value={techs}
-              onChange={e => setTechs(e.target.value)}
-              required
-            />
-          </div>
-
-          <div className="input-group">
-            <div className="input-block">
-              <label htmlFor="latitude">Latitude</label>
-              <input
-                name="latitude"
-                type="number"
-                id="latitude"
-                value={latitude}
-                onChange={e => setLatitude(e.target.value)}
-                required
-              />
-            </div>
-
-            <div className="input-block">
-              <label htmlFor="longitude">Longitude</label>
-              <input
-                name="longitude"
-                type="number"
-                id="longitude"
-                value={longitude}
-                onChange={e => setLongitude(e.target.value)}
-                required
-              />
-            </div>
-          </div>
-
-          <button type="submit">Salvar</button>
-        </form>
+        <DevForm onSubmit={handleAddDev} />
       </aside>
       <main>
         <ul>
-          <li className="dev-item">
-            <header>
-              <img
-                src="https://avatars3.githubusercontent.com/u/25853108?s=460&v=4"
-                alt="Lucas Ferrar"
-              />
-              <div className="user-info">
-                <strong>Lucas Ferrari</strong>
-                <span>ReactJS, React Native, Node.JS</span>
-              </div>
-            </header>
-            <p>Web Developer in training</p>
-            <a href="https://github.com/0lucasferrari">
-              Acessar perfil no Github
-            </a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img
-                src="https://avatars3.githubusercontent.com/u/25853108?s=460&v=4"
-                alt="Lucas Ferrar"
-              />
-              <div className="user-info">
-                <strong>Lucas Ferrari</strong>
-                <span>ReactJS, React Native, Node.JS</span>
-              </div>
-            </header>
-            <p>Web Developer in training</p>
-            <a href="https://github.com/0lucasferrari">
-              Acessar perfil no Github
-            </a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img
-                src="https://avatars3.githubusercontent.com/u/25853108?s=460&v=4"
-                alt="Lucas Ferrar"
-              />
-              <div className="user-info">
-                <strong>Lucas Ferrari</strong>
-                <span>ReactJS, React Native, Node.JS</span>
-              </div>
-            </header>
-            <p>Web Developer in training</p>
-            <a href="https://github.com/0lucasferrari">
-              Acessar perfil no Github
-            </a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img
-                src="https://avatars3.githubusercontent.com/u/25853108?s=460&v=4"
-                alt="Lucas Ferrar"
-              />
-              <div className="user-info">
-                <strong>Lucas Ferrari</strong>
-                <span>ReactJS, React Native, Node.JS</span>
-              </div>
-            </header>
-            <p>Web Developer in training</p>
-            <a href="https://github.com/0lucasferrari">
-              Acessar perfil no Github
-            </a>
-          </li>
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev} />
+          ))}
         </ul>
       </main>
     </div>
