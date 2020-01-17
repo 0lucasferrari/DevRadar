043544fb3f72573f397dc355c06339a644cbc30b
0lucasferrari <lucas.p.f.carvalho@gmail.com>
2020-01-16 21:43:17 -0300

    Construindo a interface web - parte 2

--- web/src/App.js
+++ web/src/App.js
@@ -1,39 +1,177 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import api from './services/api';
 import './global.css';
 import './App.css';
 import './Sidebar.css';
+import './Main.css';
 
 function App() {
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
+  const [github_username, setGithubUsername] = useState('');
+  const [techs, setTechs] = useState('');
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        const { latitude, longitude } = position.coords;
+
+        setLatitude(latitude);
+        setLongitude(longitude);
+      },
+      err => {
+        console.log(err);
+      },
+      {
+        timeout: 30000
+      }
+    );
+  }, []);
+
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get('dev');
+    }
+  }, []);
+
+  async function handleAddDev(e) {
+    e.preventDefault();
+
+    const response = await api.post('/devs', {
+      github_username,
+      techs,
+      latitude,
+      longitude
+    });
+
+    setGithubUsername('');
+    setTechs('');
+  }
+
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form>
-          <div class="input-block">
+        <form onSubmit={handleAddDev}>
+          <div className="input-block">
             <label htmlFor="github_username">Usuário do Github</label>
-            <input name="github_username" id="github_username" required />
+            <input
+              name="github_username"
+              id="github_username"
+              value={github_username}
+              onChange={e => setGithubUsername(e.target.value)}
+              required
+            />
           </div>
 
-          <div class="input-block">
+          <div className="input-block">
             <label htmlFor="techs">Tecnologias</label>
-            <input name="techs" id="techs" required />
+            <input
+              name="techs"
+              id="techs"
+              value={techs}
+              onChange={e => setTechs(e.target.value)}
+              required
+            />
           </div>
 
           <div className="input-group">
-            <div class="input-block">
-              <label htmlFor="longitude">Longitude</label>
-              <input name="longitude" id="longitude" required />
-            </div>
-            <div class="input-block">
+            <div className="input-block">
               <label htmlFor="latitude">Latitude</label>
-              <input name="latitude" id="latitude" required />
+              <input
+                name="latitude"
+                type="number"
+                id="latitude"
+                value={latitude}
+                onChange={e => setLatitude(e.target.value)}
+                required
+              />
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input
+                name="longitude"
+                type="number"
+                id="longitude"
+                value={longitude}
+                onChange={e => setLongitude(e.target.value)}
+                required
+              />
             </div>
           </div>
-        </form>
 
-        <button type="submit">Salvar</button>
+          <button type="submit">Salvar</button>
+        </form>
       </aside>
-      <main></main>
+      <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars3.githubusercontent.com/u/25853108?s=460&v=4"
+                alt="Lucas Ferrar"
+              />
+              <div className="user-info">
+                <strong>Lucas Ferrari</strong>
+                <span>ReactJS, React Native, Node.JS</span>
+              </div>
+            </header>
+            <p>Web Developer in training</p>
+            <a href="https://github.com/0lucasferrari">
+              Acessar perfil no Github
+            </a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars3.githubusercontent.com/u/25853108?s=460&v=4"
+                alt="Lucas Ferrar"
+              />
+              <div className="user-info">
+                <strong>Lucas Ferrari</strong>
+                <span>ReactJS, React Native, Node.JS</span>
+              </div>
+            </header>
+            <p>Web Developer in training</p>
+            <a href="https://github.com/0lucasferrari">
+              Acessar perfil no Github
+            </a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars3.githubusercontent.com/u/25853108?s=460&v=4"
+                alt="Lucas Ferrar"
+              />
+              <div className="user-info">
+                <strong>Lucas Ferrari</strong>
+                <span>ReactJS, React Native, Node.JS</span>
+              </div>
+            </header>
+            <p>Web Developer in training</p>
+            <a href="https://github.com/0lucasferrari">
+              Acessar perfil no Github
+            </a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars3.githubusercontent.com/u/25853108?s=460&v=4"
+                alt="Lucas Ferrar"
+              />
+              <div className="user-info">
+                <strong>Lucas Ferrari</strong>
+                <span>ReactJS, React Native, Node.JS</span>
+              </div>
+            </header>
+            <p>Web Developer in training</p>
+            <a href="https://github.com/0lucasferrari">
+              Acessar perfil no Github
+            </a>
+          </li>
+        </ul>
+      </main>
     </div>
   );
 }
