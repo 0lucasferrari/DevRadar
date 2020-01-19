5da50d80f4348712a0095e83580637400fff9b3c
0lucasferrari <lucas.p.f.carvalho@gmail.com>
2020-01-19 19:07:07 -0300

    Funcionalidades avançadas

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -14,6 +14,7 @@ import {
 } from 'expo-location';
 import { MaterialIcons } from '@expo/vector-icons';
 import api from '../services/api';
+import { connect, disconnect, subscribeToNewDevs } from '../services/socket';
 
 function Main({ navigation }) {
   const [devs, setDevs] = useState([]);
@@ -43,6 +44,17 @@ function Main({ navigation }) {
     loadInitialPosition();
   }, []);
 
+  useEffect(() => {
+    subscribeToNewDevs(dev => setDevs([...devs, dev]));
+  }, [devs]);
+
+  function setupWebsocket() {
+    disconnect();
+
+    const { latitude, longitude } = currentRegion;
+    connect(latitude, longitude, techs);
+  }
+
   async function loadDevs() {
     const { latitude, longitude } = currentRegion;
 
@@ -53,7 +65,9 @@ function Main({ navigation }) {
         techs
       }
     });
+
     setDevs(response.data.devs);
+    setupWebsocket();
   }
 
   function handleRegionChanged(region) {
